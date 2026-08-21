80c7b7ae3df87291d9c27d0ac19a9541931bffd5
13555853258 <3933400591@qq.com>
2026-08-21 10:10:48 +0800

    feat: return reservation reader and book details

--- library-circulation-service/src/main/java/com/library/circulation/ReservationMapper.java
+++ library-circulation-service/src/main/java/com/library/circulation/ReservationMapper.java
@@ -1,1 +1,33 @@
-package com.library.circulation; import com.baomidou.mybatisplus.core.mapper.BaseMapper; import org.apache.ibatis.annotations.Mapper; @Mapper public interface ReservationMapper extends BaseMapper<Reservation>{}
+package com.library.circulation;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface ReservationMapper extends BaseMapper<Reservation> {
+    @Select("""
+        SELECT
+            rv.id,
+            rv.reader_id AS readerId,
+            r.reader_no AS readerNo,
+            u.real_name AS realName,
+            rv.book_id AS bookId,
+            b.title AS bookTitle,
+            b.isbn,
+            rv.queue_no AS queueNo,
+            rv.reserve_time AS reserveTime,
+            rv.expire_time AS expireTime,
+            rv.status,
+            rv.notified
+        FROM reservation rv
+        JOIN reader r ON rv.reader_id = r.id
+        JOIN sys_user u ON r.user_id = u.id
+        JOIN book b ON rv.book_id = b.id
+        ORDER BY rv.id DESC
+        """)
+    List<Map<String, Object>> searchAll();
+}
